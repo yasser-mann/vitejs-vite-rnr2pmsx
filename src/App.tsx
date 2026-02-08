a720e51f4c24ae887e8ec7c47c8406315d4c7057
yasser-mann <yassirhssm60@gmail.com>
2026-02-08 22:18:52 +0100

    Fix unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import GuestGuide from './GuestGuide';
 import Admin from './Admin';
--- src/config.tsx
+++ src/config.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 
 // --- TYPES ---
 export interface Wifi {
